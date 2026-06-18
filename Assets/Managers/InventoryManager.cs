90b7e55ec7ae3dca98e5568b91eff165e39240e9
ReganUA <162838412+ReganUA@users.noreply.github.com>
2026-06-18 10:01:54 +0200

    Added inventory manager

--- Assets/Managers/InventoryManager.cs
+++ Assets/Managers/InventoryManager.cs
@@ -7,6 +7,8 @@ public class InventoryManager : MonoBehaviour
 
     public List<GameObject> heldItems = new();
     public List<GameObject> itemsDisplayed = new();
+
+    public List<List<AbilitySO>> SavedWeapons = new();
     void Awake()
     {
         if (instance == null)
@@ -18,5 +20,19 @@ public class InventoryManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    public void SaveWeapon(List<AbilitySO> weapon)
+    {
+        SavedWeapons.Add(weapon);
+    }
+    public void GetWeapon(Unit target, ComponentRuntimeStats statsCarrier, int idx)
+    {
+        target.Abilities.Clear();
+        List<Ability> createdAbilities = new List<Ability>();
+        for (int i = 0; i < createdAbilities.Count; i++)
+        {
+            createdAbilities[i] = SavedWeapons[idx][i].CreateAbility(statsCarrier);
+        }
+        target.Abilities = createdAbilities;
     }
 }
